77aefda35c9c297dc1eefadae4b79cda9ba3a3fc
MrKamake <seongjooon@gmail.com>
2019-06-11 18:19:43 +0900

    complete

--- app/index.js
+++ app/index.js
@@ -2,92 +2,93 @@
 import 'styles/index.css';
 
 (function () {
-  let scoreBoard = 0;
-  let count = 10;
   let beforeId;
+  let scoreBoard = 0;
+  let remainedCount = 10;
+  let isGameStarted = false;
 
-  let stage = [];
-  const stage1 = Array(9).fill(null).map((_, i) => ({ id: i, isUp: false}));
+  const holeStage = Array(9).fill(null).reduce((acc, _, i) => {
+    const innerArr = Math.floor(i / 3);
+    if (acc[innerArr] && acc[innerArr].length < 3) acc[innerArr].push({ id: i, isUp: false });
+    if (!acc[innerArr]) acc[innerArr] = [{ id: i, isUp: false }];
+    return acc;
+  }, []);
 
-  for (let i = 0; i < 3; i++) {
-    stage.push(stage1.splice(0, 3));
-  }
-  document.getElementById("punch-count").innerHTML = `Count : ${count}`;
   const getRandomNumber = () => Math.floor(Math.random() * 3);
 
-  const punchMole = (e) => {
-    count--;
-    document.getElementById("punch-count").innerHTML = `Count : ${count}`;
-    if (count === 0 && scoreBoard === 0) {
-      for (let i = 0; i < 9; i++) {
-        document.getElementById(`hole-${i}`).removeEventListener('click', punchMole);
-        document.getElementById(`hole-${i}`).src = 'assets/images/hole.jpg';
-      }
-      getRandomMole = null;
-      document.getElementById("score-board").innerHTML = `Your Punch Score : ${(scoreBoard / 10) * 100}`;
-      document.getElementById("restartBtn").style.display = 'inline';
-    } else if (count === 0 && scoreBoard > 0 && scoreBoard < 10) {
-      for (let i = 0; i < 9; i++) {
-        document.getElementById(`hole-${i}`).removeEventListener('click', punchMole);
-        document.getElementById(`hole-${i}`).src = 'assets/images/hole.jpg';
-      }
-      getRandomMole = null;
-      document.getElementById("score-board").innerHTML = `Your Punch Score : ${(scoreBoard / 10) * 100}`;
-      document.getElementById("restartBtn").style.display = 'inline';
-    } else if (count === 0) {
-      for (let i = 0; i < 9; i++) {
-        document.getElementById(`hole-${i}`).removeEventListener('click', punchMole);
-        document.getElementById(`hole-${i}`).src = 'assets/images/hole.jpg';
-      }
-      getRandomMole = null;
-      document.getElementById("score-board").innerHTML = `Your Punch Score : ${((scoreBoard + 1) / 10) * 100}`;
-      document.getElementById("restartBtn").style.display = 'inline';
-    }
-
-    const id = e.target.id.split('-')[1] * 1;
-    const flatten = stage.reduce((acc, v) => {
-      return [...acc, ...v];
-    }, []);
-    const targetMole = flatten.filter(v => v.id === id)[0];
-
-    if (!targetMole.isUp) return;
-    document.getElementById(e.target.id).src = 'assets/images/hole.jpg';
-    scoreBoard++;
-    targetMole.isUp = false;
-    setTimeout(getRandomMole, 1000);
+  const checkCount = () => {
+    if (remainedCount !== 0) return;
+    gameOver();
   };
 
-  let getRandomMole = () => {
-    let randomMole = stage[getRandomNumber()][getRandomNumber()];
-    if (beforeId === randomMole.id) return getRandomMole();
-    randomMole.isUp = true;
-    beforeId = randomMole.id;
-    document.getElementById(`hole-${randomMole.id}`).src = 'assets/images/mole.jpg';
-    setTimeout(() => { hideMole(randomMole.id) }, 3000);
+  const punchMole = e => {
+    remainedCount--;
+    document.getElementById("punch-count").innerHTML = `Count : ${remainedCount}`;
+
+    const id = e.target.id.split('-')[1] * 1;
+    const flatten = holeStage.reduce((acc, v) => { return [...acc, ...v] }, []);
+    const selectedHole = flatten.filter(v => v.id === id)[0];
+
+    if (!selectedHole.isUp) return checkCount();
+    document.getElementById(e.target.id).src = 'assets/images/hole.jpg';
+    scoreBoard++;
+    selectedHole.isUp = false;
+
+    if (isGameStarted) setTimeout(riseMole, 1000);
+    checkCount();
+  };
+
+  let riseMole = () => {
+    let selectedHole = holeStage[getRandomNumber()][getRandomNumber()];
+
+    if (beforeId === selectedHole.id) return riseMole();
+    selectedHole.isUp = true;
+    beforeId = selectedHole.id;
+    document.getElementById(`hole-${selectedHole.id}`).src = 'assets/images/mole.jpg';
+
+    setTimeout(() => { hideMole(selectedHole.id) }, 3000);
   };
 
   const hideMole = id => {
-    const flatten = stage.reduce((acc, v) => {
-      return [...acc, ...v];
-    }, []);
-    const targetMole = flatten.filter(v => v.id === id)[0];
-    if (!targetMole.isUp) return;
+    const flatten = holeStage.reduce((acc, v) => { return [...acc, ...v] }, []);
+    const selectedHole = flatten.filter(v => v.id === id)[0];
+
+    if (!selectedHole.isUp) return;
     document.getElementById(`hole-${id}`).src = 'assets/images/hole.jpg';
-    targetMole.isUp = false;
-    setTimeout(getRandomMole, 1000);
+    selectedHole.isUp = false;
+
+    if (isGameStarted) setTimeout(riseMole, 1000);
+  };
+
+  const gameOver = () => {
+    isGameStarted = false;
+
+    for (let i = 0; i < 9; i++) {
+      document.getElementById(`hole-${i}`).removeEventListener('click', punchMole);
+    }
+
+    document.getElementById("score-board").style.display = 'inline';
+    document.getElementById("score-board").innerHTML = `Your Punch Score : ${(scoreBoard / 10) * 100}`;
+    document.getElementById("restartBtn").style.display = 'inline';
   };
 
   const gameStart = () => {
+    isGameStarted = true;
+    scoreBoard = 0;
+    remainedCount = 10;
+
+    document.getElementById("score-board").style.display = 'none';
+    document.getElementById("restartBtn").style.display = 'none';
+    document.getElementById("punch-count").innerHTML = `Count : ${remainedCount}`;
+
     for (let i = 0; i < 9; i++) {
       document.getElementById(`hole-${i}`).addEventListener('click', punchMole);
     }
 
     document.getElementById("startBtn").style.display = 'none';
-    setTimeout(getRandomMole, 1000);
+    setTimeout(riseMole, 1000);
   };
 
-  const reload = () => location.reload();
-
   document.getElementById('startBtn').addEventListener('click', gameStart);
-  document.getElementById('restartBtn').addEventListener('click', reload);
+  document.getElementById('restartBtn').addEventListener('click', gameStart);
 })();
